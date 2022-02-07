22d77746a8b340880a51ba466fdbf4a8c3516c55
Pedro Paulo <pedropaulolucas@hotmail.com>
2022-02-07 18:53:57 -0300

    added custom characters to password filters, added button to copy to clipboard

--- script.js
+++ script.js
@@ -1,42 +1,94 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     document.querySelector('#gerenate-password').addEventListener('click', function () {
         const filtersChecked = document.querySelectorAll('[name="filters"]:checked');
-        const passwordLength = +(document.querySelector('#password-length').value);
+        const passwordLength = +document.querySelector('#length').value;
+        const hasExcludedCharacters = Boolean(document.querySelector('#exclude-characters:checked'));
+
         let filters = []
 
-        for (input of filtersChecked)
-            filters.push(input.value);
+        for (let filter of filtersChecked)
+            filters.push(filter.value);
 
-        document.querySelector('#password').innerHTML = createPassword({ filters, passwordLength });
+        const generatePassword = configPassword({ filters, passwordLength, hasExcludedCharacters });
+
+        document.querySelector('#password').innerHTML = generatePassword();
     })
 
-    const optionsPassword = {
-        uppercase() {
-            return String.fromCharCode(Math.floor(Math.random() * (26)) + 65);
+    document.querySelector('#copy-password').addEventListener('click', function () {
+        const password = document.getElementById('password');
+        if (!password.innerHTML) return;
+
+        if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+            return navigator.clipboard.writeText(password.innerHTML);
+    });
+
+    const passwordOptions = {
+        characters: {
+            downcase: 'abcdefghijklmnopqrstuvwxyz',
+            uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            numbers: '0123456789',
         },
-        downcase() {
-            return String.fromCharCode(Math.floor(Math.random() * (26)) + 97);
+        excludeCharacters(excludedCharacters, characters) {
+            for (let character in characters) {
+                excludedCharacters.map(characterExcluded => {
+                    characters[character] = characters[character].replaceAll(characterExcluded, '');
+                });
+            }
+            return characters;
+        },
+        randomUppercase() {
+            const uppercaseCharacters = this.characters.uppercase;
+            return uppercaseCharacters[Math.floor(uppercaseCharacters.length * Math.random())];
+        },
+        randomDowncase() {
+            const downcaseCharacters = this.characters.downcase;
+            return downcaseCharacters[Math.floor(downcaseCharacters.length * Math.random())];
+        },
+        letters() {
+            const options = document.querySelector('#letters-options');
+            switch (options.value) {
+                case 'uppercase':
+                    return this.randomUppercase();
+                case 'mixed':
+                    return Math.floor(Math.random() * 10) % 2 === 0 ?
+                        this.randomUppercase() : this.randomDowncase();
+                default:
+                    return this.randomDowncase();
+            }
         },
         numbers() {
-            return Math.floor(Math.random() * (10));
+            const numbers = this.characters.numbers;
+            return numbers[Math.floor(numbers.length * Math.random())];
         },
-        specialCharacters() {
-            let characters = "!@#$%¨&*(){}`^~<>,.;:/*-+\|";
-            return characters[Math.floor(characters.length * Math.random())];
-        }
+        customCharacters() {
+            const customCharacters = document.querySelector('#characters-custom').value;
+            return customCharacters[Math.floor(customCharacters.length * Math.random())];
+        },
     }
 
 
-    function createPassword(options) {
-        console.log(options);
-        const { filters, passwordLength = 8 } = options;
+    function configPassword(options) {
+        const { filters, passwordLength, hasExcludedCharacters } = options;
+        const characters = {
+            downcase: 'abcdefghijklmnopqrstuvwxyz',
+            uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            numbers: '0123456789',
+        }
         let password = '';
+
+        const excludedCharacters = hasExcludedCharacters ?
+            document.querySelector('#excluded-characters').value.split('') : [];
+
+        passwordOptions.characters = excludedCharacters ?
+            passwordOptions.excludeCharacters(excludedCharacters, characters) : characters;
+
         for (let i = 0; i < passwordLength; i++) {
             const randomFilter = Math.floor(Math.random() * (filters.length));
             const filter = filters[randomFilter];
-            password += filter ? optionsPassword[filter]() : optionsPassword.downcase();
+            password += filter ?
+                passwordOptions[filter](excludedCharacters) : passwordOptions.letters(excludedCharacters);
         }
-        return password;
+
+        return () => password;
     }
 });
